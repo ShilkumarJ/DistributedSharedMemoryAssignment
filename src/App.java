1056af1dffeb24d4b02018f2a29c2cb92aee413f
ShilkumarJ <shilkumarjadhav@gmail.com>
2017-01-21 18:27:57 +0530

    Refactored server code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class App {
-    public static void main(String args[]) {
-
+    public static void main(String args[]) throws IOException {
+        GossipServer gossipServer = new GossipServer();
+        gossipServer.start( 3 );
     }
 }
--- src/GossipServer.java
+++ src/GossipServer.java
@@ -1,85 +1,86 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GossipServer {
-    static Map<Socket, PrintWriter> socketPrintWriterMap = new HashMap<>(  );
-    public static void main( String[] args ) throws Exception {
-        BufferedReader keyRead = new BufferedReader( new InputStreamReader( System.in ) );
-        ServerSocket sersock = new ServerSocket( 3000 );
-        System.out.println( "Server  ready for chatting" );
-        Socket sock1 = sersock.accept();
-        Socket sock2 = sersock.accept();
-        Socket sock3 = sersock.accept();
-        Socket sock4 = sersock.accept();
+    static Map<Socket, PrintWriter> socketPrintWriterMap = new HashMap<>();
+    static List<Socket> sockets = new ArrayList<>();
+    private ServerSocket serSock;
 
-        createThread( sock1, keyRead );
-        createThread( sock2, keyRead );
-        createThread( sock3, keyRead );
-        createThread( sock4, keyRead );
+    public GossipServer() {
+        createSocket();
+    }
+
+    public static void main( String[] args ) throws Exception {
+
+    }
+
+    private void listenTo( final Socket sock ) {
+        new Thread( () -> {
+
+            BufferedReader receiveRead = null;
+            String receiveMessage;
+
+            try {
+                InputStream istream = sock.getInputStream();
+                receiveRead = new BufferedReader( new InputStreamReader( istream ) );
+                if ( ( receiveMessage = receiveRead.readLine() ) != null ) {
+                    System.out.println( sock.getPort() + "  " + receiveMessage );
+                }
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+        } ).start();
+    }
+
+    private void createOutputStreamFor( Socket sock ) throws IOException {
+        OutputStream outputStream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter( outputStream, true );
+        socketPrintWriterMap.put( sock, pwrite );
+    }
+
+    private void createSocket() {
+        try {
+            serSock = new ServerSocket( 3000 );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void start( int numberOfNodes ) throws IOException {
+        for ( int i = 0; i < numberOfNodes; i++ ) {
+            Socket socket = serSock.accept();
+            sockets.add( socket );
+            listenTo( socket );
+            createOutputStreamFor( socket );
+
+        }
+        startWriteThread();
+    }
+
+    private void startWriteThread() {
+        BufferedReader keyRead = new BufferedReader( new InputStreamReader( System.in ) );
 
         String sendMessage;
         while ( true ) {
             try {
                 sendMessage = keyRead.readLine();
-                int  client = Integer.parseInt(  sendMessage.split( " " )[0]);
-                String msg =  sendMessage.split( " " )[1];
-                PrintWriter pwrite = null;
-
-                switch ( client ){
-                    case 1:
-                        pwrite = socketPrintWriterMap.get(sock1);
-                        break;
-                    case 2:
-                        pwrite = socketPrintWriterMap.get(sock2);
-                        break;
-                    case 3:
-                        pwrite = socketPrintWriterMap.get(sock3);
-                        break;
-                    case 4:
-                        pwrite = socketPrintWriterMap.get(sock4);
-                        break;
-                }
-
-                pwrite.println( sendMessage );
-                pwrite.flush();
+                int clientNumber = Integer.parseInt( sendMessage.split( " " )[0] );
+                String msg = sendMessage.split( " " )[1];
+                PrintWriter printWriter = null;
+                printWriter = socketPrintWriterMap.get( sockets.get( clientNumber - 1 ) );
+                printWriter.println( msg );
+                printWriter.flush();
             } catch ( IOException e ) {
                 e.printStackTrace();
             }
 
         }
-
-    }
-
-    private static void createThread( final Socket sock, BufferedReader keyRead ) {
-        new Thread( new Runnable() {
-            @Override
-            public void run() {
-                System.out.println( "port   " + sock.getPort() );
-
-                OutputStream ostream = null;
-                BufferedReader receiveRead = null;
-                PrintWriter pwrite = null;
-                String receiveMessage;
-
-                try {
-                    ostream = sock.getOutputStream();
-                    pwrite = new PrintWriter( ostream, true );
-                    socketPrintWriterMap.put( sock, pwrite );
-                    System.out.println("map"+sock.getPort());
-                    InputStream istream = sock.getInputStream();
-                    receiveRead = new BufferedReader( new InputStreamReader( istream ) );
-                    if ( ( receiveMessage = receiveRead.readLine() ) != null ) {
-                        System.out.println( sock.getPort()+"  "+ receiveMessage );
-                    }
-                } catch ( IOException e ) {
-                    e.printStackTrace();
-                }
-
-            }
-        } ).start();
     }
 }
